c09419647a23b27f83a773aeb4c1e23b875703ab
Oleg Keene <ol.keene@gmail.com>
2015-12-21 22:20:01 +0200

    mailer

---
+++ config/initializers/mailer.rb
@@ -0,0 +1,12 @@
+if Rails.env.staging? || Rails.env.production?
+  ActionMailer::Base.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => "knomsy-#{Rails.env}.heroku.com",
+    :authentication => :plain,
+  }
+  
+  ActionMailer::Base.delivery_method = :smtp
+end
