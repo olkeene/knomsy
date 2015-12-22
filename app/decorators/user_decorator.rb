1f5c68bdacd8b27872cc1a74f7ecfecadf866e8d
Oleg Keene <ol.keene@gmail.com>
2015-12-22 16:38:35 +0200

    cleanup

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,16 +1,6 @@
 class UserDecorator < Draper::Decorator
   delegate_all
   
-  # def location?
-  #   [country, city].any?{|v| !v.blank? }
-  # end
-  
-  # def location
-  #   return unless location?
-  #   
-  #   [country, city].reject(&:blank?).join(', ')
-  # end
-  
   def cover_url
     # return unless cover
     
