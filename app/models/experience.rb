ea9b83aa6c8e5f2eee56e3c7996c849245d5deb6
Oleg Keene <ol.keene@gmail.com>
2015-12-29 12:07:22 +0200

    validates presence of started_at and ended_at on experiences

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -6,10 +6,12 @@ class Experience < ActiveRecord::Base
   validates :user_id, :role, presence: true
   validates :company_name, :title, length: {minimum: 3, maximum: 255}
   validates :description, length: {maximum: 500}, allow_blank: true
-  validates :started_at, :ended_at, date: true
+  validates :started_at, :ended_at, date: true, presence: true
   
   [:started_at, :ended_at].each do |attr|
     define_method "#{attr}=" do |v|
+      return if v.blank?
+      
       begin
         write_attribute attr, Date.strptime(v, '%m/%d/%Y')
       rescue ArgumentError
