1f67c534b171a4bb56aa1f9707a11314d3d5cb13
Oleg Keene <ol.keene@gmail.com>
2016-01-21 21:27:50 +0200

    random references

--- app/assets/javascripts/components/user_profile/content/references.js.coffee
+++ app/assets/javascripts/components/user_profile/content/references.js.coffee
@@ -2,25 +2,25 @@
 
 @UserProfile_Content_References = React.createFactory React.createClass
   render: ->
-    references = _.map [1, 2], (ref, i)->
+    references = _.map @props.user.references, (ref, i)->
       (div className: "view-body", key: "ref_#{i}",
         (div className: "view-body__media media",
           (div className: "media-left",
             (a null, 
-              (img className: "media__img media-object", src: 'https://placehold.it/50x50')))
+              (img className: "media__img media-object", src: ref.user_image_url)))
 
           (div className: "media-body",
-            (h4 className: "media__title media-heading", 'Knomsy')
-            (p className: "media__text", 'Startup eco system')
-            (p className: "media__name", 'Denis Kozminikh')
+            (h4 className: "media__title media-heading", ref.company_name)
+            (p className: "media__text", ref.description)
+            (p className: "media__name", ref.user_name)
             (span className: "media__text", 'says:')))
 
         (div className: "view-body_references",
           (div className: "view-body__quotes_open")
-          (p null, 'content.....')
+          (p null, ref.content)
           (div className: "view-body__quotes_close")))
       
     (div className: "content__section",
       (h3 className: "section__title", 'References')
       (div className: "section__body section__body_view",
-        (references)))
+        references))
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -10,4 +10,29 @@ class UserSerializer < ActiveModel::Serializer
     
     has_one  :investing
     has_many :experiences
+    has_many :references
+    
+  def references
+    all = [
+      'Thinking of how to find adepts/team members yet not sure how to create the team and motivate them? With Knomsy it all becomes clear, structured simply put achievable.',
+      'You are constantly getting barraged with investment proposals and are finding it increasingly difficult wade through the hundreds of projects and find the diamond in the rough? To investors we offers an innovative yet simple time saving solution.',
+      'In order to generate traction one has to fully understand their market and intended user base. Wouldn\'t it be great if there was an innovative product that would took care of this from the start?',
+      'Are you having trouble putting together a clear picture of what investors are looking for? Knomsy has a solution for that.',
+      'Thinking of how to find adepts/team members yet not sure how to create the team and motivate them? With Knomsy it all becomes clear, structured simply put achievable.',
+      'You are constantly getting barraged with investment proposals and are finding it increasingly difficult wade through the hundreds of projects and find the diamond in the rough? To investors we offers an innovative yet simple time saving solution.',
+      'In order to generate traction one has to fully understand their market and intended user base. Wouldn\'t it be great if there was an innovative product that would took care of this from the start?',
+      'Thinking of how to find adepts/team members yet not sure how to create the team and motivate them? With Knomsy it all becomes clear, structured simply put achievable.',
+      'Knomsy has now moved forward to automated data analyses and processing as well as index calculation. The tech part of the project is being developed by using the most advanced programing technologies. In the nearest future we are planning to announce the beta release version for testing. We will also be connecting the second part of our data processing which is run through accessory influence.'
+    ]
+    
+    all.sample(rand(2) + 1).map{|h| 
+      {
+        content: h, 
+        user_name:    'Denis Kozminikh',
+        user_image_url: 'http://knomsy.com/assets/landing/team/team-1-977563e716b16ac4ada5ae10679e8ffd.png'
+        company_name: 'Knomsy',
+        description:  'Startup eco system'
+      } 
+    }
+  end
 end
