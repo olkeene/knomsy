8d44d182ba1d451c6d7c894d98b9954b08688095
Oleg Keene <ol.keene@gmail.com>
2016-02-04 10:49:02 +0200

    login require for dashboard

--- app/controllers/user/dashboards_controller.rb
+++ app/controllers/user/dashboards_controller.rb
@@ -1,5 +1,7 @@
 # Public pages, login not required
 class User::DashboardsController < ApplicationController
+  before_action :authenticate_user!
+  
   def show
     @companies = current_user.companies
   end
