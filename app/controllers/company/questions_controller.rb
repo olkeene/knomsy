3061510dbc94ec41843c8be628a56a0b8b309eac
Oleg Keene <ol.keene@gmail.com>
2016-02-14 14:15:11 +0200

    prevent questions loop

--- app/controllers/company/questions_controller.rb
+++ app/controllers/company/questions_controller.rb
@@ -1,7 +1,9 @@
 class Company::QuestionsController < Company::BaseController
   def next
-    question = SurveyQuestion.left_for(current_user, @company, params[:skip_id]).first
-    question = question.decorate(context: {company: @company}) if question
+    question = if Ability.new(current_user).can?(:took_survey, @company)
+      q = SurveyQuestion.left_for(current_user, @company, params[:skip_id]).first
+      q = q.decorate(context: {company: @company}) if q
+    end
     
     render json: question
   end
