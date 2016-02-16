808bc34ad4094763d7d8924bc79dc3827fd50d1a
Oleg Keene <ol.keene@gmail.com>
2016-02-16 22:17:09 +0200

    missed short_desc in company serializer

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -1,8 +1,8 @@
 class CompanySerializer < ActiveModel::Serializer
   attributes \
-    :id,         :name,         :short_name,       :description,     :founded_on,
-    :country_id, :country_name, :city,             :location,        :market, :category_list,
-    :rating,     :rating_trend, :followings_count, :followers_count, :logo_url, :cover_url,
+    :id,         :name,         :short_name,       :description,     :short_desc, :founded_on,
+    :country_id, :country_name, :city,             :location,        :market,     :category_list,
+    :rating,     :rating_trend, :followings_count, :followers_count, :logo_url,   :cover_url,
     
     :gplay_link,    :itunes_link,  :dribbble_link, :fb_link, :gh_link, :gplus_link, 
     :linkedin_link, :twitter_link, :youtube_link,  :website,
